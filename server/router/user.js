c71ed6564b5ba856df07507758ea1dabe30b65c8
{krism01} <{krism01@naver.com}>
2022-01-31 02:40:57 +0900

    Add try catch

--- server/router/user.js
+++ server/router/user.js
@@ -1,68 +1,79 @@
-const express = require('express');
-const router = express.Router();
-const model = require('../mongoose/model');
+const Router = require('express');
+const router = Router();
+const { model } = require('../mongoose');
 const jwt = require("jsonwebtoken");
 
 // signup
 router.post("/user/signup", async (req, res) => {
-  const { nickname, email, password } = req.body;
-  // 만약 DB 에 email 이 존재하면 이미 생성된 유저 출력하기
-  const duplicationUser = await model.User.findOne({ email: email });
-  if (duplicationUser) return res.send("이미 생성된 유저");
+  try {
+    const { nickname, email, password } = req.body;
+    // 만약 DB 에 email 이 존재하면 이미 생성된 유저 출력하기
+    const duplicationUser = await model.User.findOne({ email: email });
+    if (duplicationUser) return res.status(400).send({ err: "existed user" });
+    if (!nickname | !password) return res.status(400).send({ err: "Both nickname and password is required" });
 
-  const newUser = await model.User({ nickname, email, password }).save();
-  !newUser ? res.send("회원가입 실패") : res.send(newUser);
+    const newUser = await model.User({ nickname, email, password }).save();
+    if (!newUser) return res.send({ err: "Failed to sign up" });
+    return res.send(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
 });
 
-// login - user 인증하기 위한 email, password 검사
+// login  
 router.post("/user/login", async (req, res) => {
-  const { email, password } = req.body;
-  const userEmail = await model.User.findOne({ email: email });
-  const correctUserEmail = userEmail.authenticate(password);
+  try {
+    const { email, password } = req.body;
+    const userEmail = await model.User.findOne({ email: email });
+    if (!userEmail) return res.status(400).send({ error: "존재하지 않는 이메일 입니다." });
 
-  if (!userEmail._id) {
-    return res.send({ error: true, msg: "존재하지 않는 이메일" });
-  }
+    const correctUserEmail = userEmail.authenticate(password);
+    if (!correctUserEmail) return res.status(400).send({ error: "잘못된 패스워드 입니다." });
 
-  if (!correctUserEmail) {
-    return res.send({ error: true, msg: "잘못된 패스워드" });
-  }
-
-  // server/index.js 에서 설정한 secret 을 가져온다
-  const secret = req.app.get("jwt-secret");
-  const token = jwt.sign(
-    {
-      id: userEmail._id,
+    // server/index.js 에서 설정한 secret 을 가져온다
+    const secret = req.app.get("jwt-secret");
+    const token = jwt.sign(
+      {
+        id: userEmail._id,
+        email: userEmail.email,
+        nickname: userEmail.nickname,
+      },
+      secret,
+      {
+        expiresIn: "7d",
+      },
+    );
+    // 로그인 시, 토큰을 같이 보내준다
+    return res.send({
+      msg: "로그인 성공",
       email: userEmail.email,
       nickname: userEmail.nickname,
-    },
-    secret,
-    {
-      expiresIn: "7d",
-    },
-  );
-
-  // 로그인 시, 토큰을 같이 보내준다
-  res.send({
-    error: false,
-    msg: "로그인 성공",
-    email: correctUserEmail.email,
-    nickname: correctUserEmail.nickname,
-    token: token,
-  });
+      token: token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
 });
 
 // 사용자 토큰 체크
 router.get("/user/token", async (req, res) => {
-  const { authorization } = req.headers;
-  if (!authorization) return res.send(false);
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(401).send({ err: "Unauthorized" });
 
-  const token = authorization.split(" ")[1];
-  const secret = req.app.get("jwt-secret");
+    const token = authorization.split(" ")[1];
+    const secret = req.app.get("jwt-secret");
 
-  jwt.verify(token, secret, (err, data) => {
-    err ? res.send(err) : res.send(data);
-  });
+    jwt.verify(token, secret, (err, data) => {
+      if (err) return res.status(400).send({ err: err.message })
+      return res.send(data);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
