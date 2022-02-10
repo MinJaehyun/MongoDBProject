856e8810117798a13cea611b35e87885c23f4ba3
{krism01} <{krism01@naver.com}>
2022-02-10 22:24:52 +0900

    jest 전역 설정

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "node": true,
         "browser": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true // jest is not defined 에러를 /* eslint-disable no-undef */ 대신하여 전역 설정함. 
     },
     "extends": "eslint:recommended",
     "parserOptions": {
