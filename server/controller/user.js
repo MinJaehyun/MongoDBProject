b2d999a6ebd61c47f4c8c3ffd69c53527e400e0d
{krism01} <{krism01@naver.com}>
2022-04-11 23:01:41 +0900

    상태코드 개선 및 변수명칭 변경

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,14 +4,14 @@ const jwt = require("jsonwebtoken");
 exports.signupUser = async (req, res) => {
   try {
     const { nickname, email, password } = req.body;
-    if (!nickname || !password) return res.status(400).send({ err: "Both nickname and password is required" });
+    if (!nickname || !password) return res.status(400).send({ err: "Require nickname and password" });
     // 만약 DB 에 email 이 존재하면 이미 생성된 유저 출력하기
-    const duplicationUser = await User.findOne({ email: email });
-    if (duplicationUser) return res.status(400).send({ err: "user does not exist" });
+    const user = await User.findOne({ email: email });
+    if (user) return res.status(409).send({ err: "이미 존재하는 id 입니다." });
 
     const newUser = await User({ nickname, email, password })
     await newUser.save();
-    return res.status(201).send(`Membership registration completed`);
+    return res.status(201).send(newUser);
   } catch (err) {
     console.log(err);
     return res.status(500).send({ err: err.message });
@@ -20,19 +20,19 @@ exports.signupUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const userEmail = await User.findOne({ email: email });
-    if (!userEmail) return res.status(400).send({ err: "email does not exitst" });
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).send({ err: "email does not exist" });
 
-    const correctUserEmail = userEmail.authenticate(password);
-    if (!correctUserEmail) return res.status(400).send({ err: "incorrect password" });
+    const correctUserEmail = user.authenticate(password);
+    if (!correctUserEmail) return res.status(401).send({ err: "incorrect password" });
 
     // server/index.js 에서 설정한 secret 을 가져온다
     const secret = req.app.get("jwt-secret");
     const token = jwt.sign(
       {
-        id: userEmail._id,
-        email: userEmail.email,
-        nickname: userEmail.nickname,
+        id: user._id,
+        email: user.email,
+        nickname: user.nickname,
       },
       secret,
       {
@@ -42,8 +42,8 @@ exports.loginUser = async (req, res) => {
     // 로그인 시, 토큰을 같이 보내준다
     return res.send({
       msg: "로그인 성공",
-      email: userEmail.email,
-      nickname: userEmail.nickname,
+      email: user.email,
+      nickname: user.nickname,
       token: token,
     });
   } catch (err) {
@@ -54,7 +54,6 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = async (req, res) => {
   try {
     const { email } = req.body;
-    console.log('email: ', email);
     // if (!email) return res.status(400).send({ err: "email is required" });
     const logout = await User.findOneAndDelete(email);
     return res.send(logout);
